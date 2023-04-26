c7ee2b636f79884a66227b85e738024def3c095b
MegaChuck64 <cjscottcarey@yahoo.com>
2023-04-26 19:05:00 -0400

    testing cross platform

--- src/main.c
+++ src/main.c
@@ -1,51 +1,11 @@
-#include <GLFW/glfw3.h>
-
-
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-    {
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-    }
-}
-
-void render(GLFWwindow *window)
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glBegin(GL_POLYGON);
-    glVertex3f(0.5, 0.0, 0.5);
-    glVertex3f(0.5, 0.0, 0.0);
-    glVertex3f(0.0, 0.5, 0.0);
-    glVertex3f(0.0, 0.0, 0.5);
-    glEnd();
-
-    glfwSwapBuffers(window);
-}
-
+#include <stdio.h>
 int main()
 {
-    if (!glfwInit())
-    {
-        return -1;
-    }
-
-    GLFWwindow *window = glfwCreateWindow(400, 300, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, key_callback);
-
-    while (!glfwWindowShouldClose(window))
-    {
-        render(window);
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
+    #ifdef _WIN32
+        printf("Hello, Windows!\n");
+    #elif __linux__
+        printf("Hello, Linux!\n");
+    #endif
+    
     return 0;
 }
