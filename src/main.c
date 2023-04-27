06046e6faf675d1d66dbd5d89d543afb8fff8dd5
MegaChuck64 <cjscottcarey@yahoo.com>
2023-04-27 00:05:44 -0400

    launching win window from main instead of WinMain

--- src/main.c
+++ src/main.c
@@ -1,92 +1,121 @@
 #ifdef __linux__
 
 #include <X11/Xlib.h>
+#include <X11/Xutil.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int main()
-{
-    Display *d;
-    Window w;
-    XEvent e;
-    const char *msg = "Hello, World!";
-    int s;
-
-    d = XOpenDisplay(NULL);
-    if (d == NULL)
-    {
-        fprintf(stderr, "Cannot open display\n");
-        exit(1);
-    }
-
-    s = DefaultScreen(d);
-    w = XCreateSimpleWindow(d, RootWindow(d, s), 10, 10, 100, 100, 1,
-                            BlackPixel(d, s), WhitePixel(d, s));
-    XSelectInput(d, w, ExposureMask | KeyPressMask);
-    XMapWindow(d, w);
-
-    while (1)
-    {
-        XNextEvent(d, &e);
-        if (e.type == Expose)
-        {
-            XFillRectangle(d, w, DefaultGC(d, s), 20, 20, 10, 10);
-            XDrawString(d, w, DefaultGC(d, s), 10, 50, msg, strlen(msg));
-        }
-        if (e.type == KeyPress)
-            break;
-    }
-
-    XCloseDisplay(d);
-
-    return 0;
-}
+#endif
 
 
-
-#elif _WIN32
+#ifdef _WIN32
 
 #include <windows.h>
 #include <stdio.h>
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam);
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+#endif
+
+
+int main()
 {
-    const char CLASS_NAME[] = "Window Class";
-    const char WINDOW_NAME[] = "Sample Window";
+    #ifdef __linux_
 
-    WNDCLASS wc = {0};
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME;
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW);
+    Display *display;
+    Window window;
+    XEvent event;
+    GC gc;
+    XColor red;
+    char *text = "Hello World!";
+    int s;
 
-    RegisterClass(&wc);
+    display = XOpenDisplay(NULL);
 
-    HWND hwnd = CreateWindowEx(
-        0,
-        CLASS_NAME,
-        WINDOW_NAME,
-        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-        NULL,
-        NULL,
-        hInstance,
-        NULL);
-
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0))
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+    if (display == NULL) {
+        fprintf(stderr, "Cannot open display\n");
+        exit(1);
     }
 
+    s = DefaultScreen(display);
+
+    window = XCreateSimpleWindow(display, RootWindow(display, s), 10, 10, 200, 200, 1, BlackPixel(display, s), WhitePixel(display, s));
+
+    XSelectInput(display, window, ExposureMask | KeyPressMask);
+
+    gc = XCreateGC(display, window, 0, NULL);
+
+    if (XAllocNamedColor(display, DefaultColormap(display, s), "red", &red, &red) == 0) {
+        fprintf(stderr, "Could not allocate color\n");
+        exit(1);
+    }
+
+
+    XMapWindow(display, window);
+
+    while (1) {
+        XNextEvent(display, &event);
+
+        if (event.type == Expose) {
+            XSetForeground(display, gc, BlackPixel(display, s));
+            XFillRectangle(display, window, gc, 20, 20, 40, 40);
+            XSetForeground(display, gc, red.pixel);
+            XDrawString(display, window, gc, 20, 100, text, strlen(text));
+        }
+
+        if (event.type == KeyPress) {
+            break;
+        }
+    }
+
+    XFreeGC(display, gc);
+    XDestroyWindow(display, window);
+    XCloseDisplay(display);
+
+
     return 0;
+
+    #endif
+
+    #ifdef _WIN32
+
+    FreeConsole();
+    WNDCLASS windowClass={0};
+    windowClass.hbrBackground=(HBRUSH)GetStockObject(WHITE_BRUSH);
+    windowClass.hCursor=LoadCursor(NULL, IDC_ARROW);
+    windowClass.hInstance=NULL;
+    windowClass.lpfnWndProc=WndProc;
+    windowClass.lpszClassName="Window Class";
+    if (!RegisterClass(&windowClass))
+        MessageBox(NULL, "Could not register class", "Error", MB_OK);
+    HWND windowHandle=CreateWindow("Window Class",
+                                    "Sample Window",
+                                    WS_OVERLAPPEDWINDOW,
+                                    20,
+                                    20,
+                                    400,
+                                    200,
+                                    NULL,
+                                    NULL,
+                                    NULL,
+                                    NULL);
+    ShowWindow(windowHandle, SW_RESTORE);
+    MSG messages;
+    while(GetMessage(&messages, NULL, 0, 0)>0)
+    {
+        TranslateMessage(&messages);
+        DispatchMessage(&messages);
+    }
+    DeleteObject(windowHandle); //doing it just in case
+    return messages.wParam;
+
+    #endif
 }
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+#ifdef _WIN32
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
@@ -119,7 +148,6 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
 }
-
 #endif
 
 
