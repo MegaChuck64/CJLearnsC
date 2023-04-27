1b34163f1a4e3d8766828682c87c637a5c2e5914
MegaChuck64 <cjscottcarey@yahoo.com>
2023-04-26 22:32:32 -0400

    configd win in vs tasks. opend native win32 window

--- src/main.c
+++ src/main.c
@@ -1,18 +1,12 @@
-
 #ifdef __linux__
-#include <X11/Xlib.h>
-#endif
 
+#include <X11/Xlib.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-
 int main()
 {
-#ifdef _WIN32
-    printf("Hello, Windows!\n");
-#elif __linux__
     Display *d;
     Window w;
     XEvent e;
@@ -45,10 +39,72 @@ int main()
     }
 
     XCloseDisplay(d);
-#endif
 
     return 0;
 }
+
+
+
+#elif _WIN32
+
+#include <windows.h>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    // Declare variables
+    HWND hwnd;
+    MSG msg;
+    WNDCLASS wc = {0};
+
+    // Register the window class
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+    wc.lpszClassName = "MyWindowClass";
+    RegisterClass(&wc);
+
+    // Create the window
+    hwnd = CreateWindow(
+        "MyWindowClass", "My Window",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        500, 300,
+        NULL, NULL,
+        hInstance, NULL);
+
+    // Show the window
+    ShowWindow(hwnd, nCmdShow);
+
+    // Enter the message loop
+    while(GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    // Return the exit code
+    return (int)msg.wParam;
+}
+
+// Define the window procedure
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch(uMsg)
+    {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+}
+
+
+#endif
+
+
 
 /*
     we want to support all functionality on windows and linux
